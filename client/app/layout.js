1e35e560a01bbf50d16459c78c16244fc924a6bd
shubh vaish <shubhvaish1703@gmail.com>
2026-01-14 22:39:10 +0530

    first setup commit

---
+++ client/app/layout.js
@@ -0,0 +1,24 @@
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "DrawRush - Draw & Fun",
+  description: "DrawRush - Draw & Fun",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
