bcfa45b2ecda924b2c948371e713342f29093d46
Anand Kumar Maurya <anandmaurya542@gmail.com>
2026-01-19 16:10:21 +0530

    socket-minor-changes

--- client/socket/socket.js
+++ client/socket/socket.js
@@ -1,17 +1,24 @@
 import { io } from "socket.io-client";
 
+
 let socket = null;
 
+
 // const SERVER_URL = "10.34.109.167:5000"
-const SERVER_URL = "http://localhost:5000"
+// const SERVER_URL = "10.233.251.167:5000"
+const SERVER_URL = "172.20.10.4:5000"
+// const SERVER_URL = "http://localhost:5000"
+
 
 export const initSocket = () => {
     if (!socket) {
         socket = io(SERVER_URL, {
-            transports: ["websocket"],
+            transports: ["websocket", 'polling'],
             reconnection: true,
             reconnectionAttempts: 5,
+            reconnectionDelay: 1000
         });
     }
     return socket;
 };
+
