d979c2262b95f48af93a1c3341491ef14416e46e
shubh vaish <shubhvaish1703@gmail.com>
2026-01-23 14:44:06 +0530

    server url added

--- client/socket/socket.js
+++ client/socket/socket.js
@@ -2,9 +2,7 @@ import { io } from "socket.io-client";
 
 let socket = null;
 
-// const SERVER_URL = "10.34.109.167:5000"
-// const SERVER_URL = "10.233.251.167:5000"
-const SERVER_URL = "https://drawrushserver-hosting.onrender.com"
+const SERVER_URL = "https://draw-rush-server.onrender.com"
 // const SERVER_URL = "http://localhost:5000"
 
 export const initSocket = () => {
